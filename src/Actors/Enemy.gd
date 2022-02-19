45e9cccc12ff80c303b6ccf94f40073640dfb54b
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-02-19 14:06:48 +0100

    Add stomp on enemy

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -9,3 +9,10 @@ func _physics_process(delta):
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D):
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+	
