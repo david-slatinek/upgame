6568cf3b32bbcb4249212ec0717fd2f65d1fd2d2
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-02-22 13:32:25 +0100

    Add pause

---
+++ src/UI/UserInterface.gd
@@ -0,0 +1,30 @@
+extends Control
+
+onready var scene_tree: = get_tree()
+onready var pause_overlay: ColorRect = get_node("PauseOverlay")
+onready var score: Label = get_node("Label")
+onready var paused_title: Label = get_node("PauseOverlay/Title")
+
+var paused: = false setget set_paused
+
+func _ready():
+	PlayerData.connect("score_updated", self, "update_inteface")
+	PlayerData.connect("player_died", self, "_on_PlayerData_player_died")
+	update_inteface()
+
+func update_inteface():
+	score.text = "Score: %s" % PlayerData.score
+
+func _on_PlayerData_player_died():
+	self.paused = true
+	paused_title.text = "You died"
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.paused = !paused
+		scene_tree.set_input_as_handled()
+
+func set_paused(value: bool):
+	paused = value
+	scene_tree.paused = value
+	pause_overlay.visible = value
