9a44dcd75183cefd103f5204a2c3b06786b6d1e4
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-02-22 19:36:15 +0100

    Update game gif

--- other/script.sh
+++ other/script.sh
@@ -4,8 +4,9 @@ input="video.mkv"
 output="game.gif"
 name="palette.png"
 
+filters="fps=30,scale=600:-1:flags=lanczos"
 ffmpeg -v verbose -i $input -vf palettegen $name
-ffmpeg -v verbose -i $input -vf "palettegen" -y $name
-ffmpeg -v verbose -i $input -i $name -lavfi "paletteuse" -y $output
+ffmpeg -v verbose -i $input -vf "$filters,palettegen" -y $name
+ffmpeg -v verbose -i $input -i $name -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output
 
 rm $name
